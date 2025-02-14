38fae83b1c47223e27115d6ae73ae140689415fc
kolchenkoav <kolchenkoav@gmail.com>
2025-02-14 20:39:13 +0300

    Task. 11 In progress ++

--- src/main/java/com/example/hotel_booking_service/HotelBookingServiceApplication.java
+++ src/main/java/com/example/hotel_booking_service/HotelBookingServiceApplication.java
@@ -2,6 +2,8 @@ package com.example.hotel_booking_service;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.data.mongo.MongoDataAutoConfiguration;
+import org.springframework.boot.autoconfigure.mongo.MongoAutoConfiguration;
 
 @SpringBootApplication
 public class HotelBookingServiceApplication {
--- src/main/java/com/example/hotel_booking_service/repository/StatisticRepository.java
+++ src/main/java/com/example/hotel_booking_service/repository/StatisticRepository.java
@@ -2,6 +2,10 @@ package com.example.hotel_booking_service.repository;
 
 import com.example.hotel_booking_service.entity.Statistic;
 import org.springframework.data.mongodb.repository.MongoRepository;
+import org.springframework.data.mongodb.repository.config.EnableMongoRepositories;
+import org.springframework.stereotype.Repository;
 
+@EnableMongoRepositories
+@Repository
 public interface StatisticRepository extends MongoRepository<Statistic, String> {
 }
